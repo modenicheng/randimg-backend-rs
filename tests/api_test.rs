bd6d2573751237ce095435eab6fa188b7da981f8
modenicheng <cheng20070225@outlook.com>
2026-05-31 13:22:30 +0800

    test: API integration tests

---
+++ tests/api_test.rs
@@ -0,0 +1,78 @@
+use serde_json;
+
+#[tokio::test]
+async fn test_random_image_returns_json() {
+    let client = reqwest::Client::new();
+    let resp = client
+        .get("http://localhost:8000/")
+        .query(&[("format", "json")])
+        .send()
+        .await;
+
+    match resp {
+        Ok(r) => {
+            assert!(r.status() == 200 || r.status() == 404);
+        }
+        Err(_) => {
+            // Server not running, skip
+        }
+    }
+}
+
+#[tokio::test]
+async fn test_login_returns_token() {
+    let client = reqwest::Client::new();
+    let resp = client
+        .post("http://localhost:8000/token")
+        .form(&[
+            ("username", "test"),
+            ("password", "test"),
+        ])
+        .send()
+        .await;
+
+    match resp {
+        Ok(r) => {
+            assert!(r.status() == 200 || r.status() == 401);
+        }
+        Err(_) => {}
+    }
+}
+
+#[tokio::test]
+async fn test_tags_endpoint() {
+    let client = reqwest::Client::new();
+    let resp = client
+        .get("http://localhost:8000/tags")
+        .send()
+        .await;
+
+    match resp {
+        Ok(r) => {
+            assert_eq!(r.status(), 200);
+            let body: serde_json::Value = r.json().await.unwrap();
+            assert!(body.is_array());
+        }
+        Err(_) => {}
+    }
+}
+
+#[tokio::test]
+async fn test_statistic_endpoint() {
+    let client = reqwest::Client::new();
+    let resp = client
+        .get("http://localhost:8000/statistic")
+        .send()
+        .await;
+
+    match resp {
+        Ok(r) => {
+            assert_eq!(r.status(), 200);
+            let body: serde_json::Value = r.json().await.unwrap();
+            assert!(body.get("illust_count").is_some());
+            assert!(body.get("tag_count").is_some());
+            assert!(body.get("author_count").is_some());
+        }
+        Err(_) => {}
+    }
+}
