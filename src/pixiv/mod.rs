8febd70f0735dd109dad14d1d4b642bc9dfb3d29
modenicheng <cheng20070225@outlook.com>
2026-05-31 13:29:57 +0800

    feat: add pixiv-client dependency

---
+++ src/pixiv/mod.rs
@@ -0,0 +1,4 @@
+// Pixiv client integration using the pixiv-client crate
+pub use pixiv_client::{PixivApi, PixivError, ApiResponse};
+pub use pixiv_client::config::{Config, ClientConfig};
+pub use pixiv_client::downloader::{DownloadManager, DownloadTask};
